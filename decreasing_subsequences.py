0f1ac9ebd071be8b6652545c5300f4f39f2f0830
Wei Lun <weilunn.97@gmail.com>
2020-08-23 16:45:09 +0800

    Corrections on decreasing subs and max strawberries

--- decreasing_subsequences.py
+++ decreasing_subsequences.py
@@ -3,18 +3,15 @@ from typing import List
 
 
 def decreasing_subsequences(arr: List[int]) -> int:
-    """
-    Time : O(N log N)
-    Space : O(N)
-    """
-    buckets = []
-    for a in arr:
-        pos = bisect_right(buckets, a)
-        if pos == len(buckets):
-            buckets.append(a)
+    res = []
+    for i in range(len(arr)):
+        pos = bisect_right(res, arr[i])
+        if pos == len(res):
+            res.append(arr[i])
         else:
-            buckets[pos] = a
-    return len(buckets)
+            res[pos] = arr[i]
+
+    return len(res)
 
 
 if __name__ == "__main__":
@@ -23,3 +20,5 @@ if __name__ == "__main__":
     print(decreasing_subsequences([5, 2, 4, 3, 1, 6]) == 3)
     print(decreasing_subsequences([2, 9, 12, 13, 4, 7, 6, 5, 10]) == 4)
     print(decreasing_subsequences([1, 1, 1]) == 3)
+    print(decreasing_subsequences([5, 7, 1, 5, 2, 7]) == 3)
+    print(decreasing_subsequences([5, 7, 7, 5, 5, 3, 1, 5]) == 4)
--- max_strawberries.py
+++ max_strawberries.py
@@ -6,22 +6,29 @@ def max_strawberries(arr: List[int], num: int) -> int:
     Time  : O(MN)
     Space : O(MN), where M = len(arr) and N = num
     """
-    # DP[I][J] = MAX STRAWBERRIES GIVEN ARR[:I+1] AND NUM = J
     nrows, ncols = len(arr) + 1, num + 1
+
+    # LET DP[I][J] = MAX STRAWBERRIES GIVEN ARR[:I] AND NUM = J
     dp = [[0 for _ in range(ncols)] for _ in range(nrows)]
 
-    # ROW 1
-    for j in range(1, ncols):
-        dp[1][j] = arr[0] + dp[0][j - arr[0]] if arr[0] <= j else 0
+    # GET THE MAXIMUM
+    max_straw = 0
 
-    # OTHER CELLS
+    # FILL ROW 1
+    for j in range(1, ncols):
+        dp[1][j] = arr[0] if arr[0] <= j else 0
+        max_straw = max(max_straw, dp[1][j])
+
+    # FILL OTHER CELLS
     for i in range(2, nrows):
         for j in range(1, ncols):
-            take = arr[i - 1] + dp[i - 2][j - arr[i - 1]] if arr[i - 1] <= j else 0
+            # PICK IT IFF THERE'S SPACE
+            pick = dp[i - 2][j - arr[i - 1]] + arr[i - 1] if arr[i - 1] <= j else 0
             leave = dp[i - 1][j]
-            dp[i][j] = max(take, leave)
+            dp[i][j] = max(pick, leave)
+            max_straw = max(max_straw, dp[i][j])
 
-    return dp[-1][-1]
+    return max_straw
 
 
 if __name__ == "__main__":
